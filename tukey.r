a501bf48abd781fbc0c9831b5f4a74160b8e3e5d
kisudsoe <kisudsoe@gmail.com>
2016-04-26 14:47:46 +0900

    tukey 함수
    최초 작성 버전

---
+++ tukey.r
@@ -0,0 +1,27 @@
+
+	row = rownames(data)
+	result = 0
+	out = ""
+
+	n = length(row)-1
+	groups = as.character(t(data)[,1]) # data.frame to vector by row
+
+		values = as.numeric(t(data)[,i+1])
+
+		# post hoc tests - tukey #
+		a1 = aov(values~groups)
+		posthoc = TukeyHSD(x=a1, conf.level=0.95) # 95% confidence interval
+		plot(posthoc) # generate graph
+		text(0,10.7,cex=1.2,labels=row[i+1],xpd=TRUE) # ID on the graph
+
+		# using multcompView package #
+		tmp = extract_p(posthoc$groups)
+		sigroup = multcompLetters(tmp)
+
+		if(i==1) {
+			out = cbind(row[i+1],t(data.frame(sigroup$Letters)))
+		} else {
+			tmp2 = cbind(row[i+1],t(data.frame(sigroup$Letters)))
+			out = cbind(out,tmp2)
+		}
+	return(out)
