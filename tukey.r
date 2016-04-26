b497f07ea9bce939992a98326445065e14a65a82
kisudsoe <kisudsoe@gmail.com>
2016-04-26 17:19:45 +0900

    tukey 함수_수정

--- tukey.r
+++ tukey.r
@@ -1,3 +1,5 @@
+﻿ss.tukey = function(data) { # function 'ss.tukey' start
+	library(multcompView) # package multcompView install needed
 
 	row = rownames(data)
 	result = 0
@@ -6,6 +8,7 @@
 	n = length(row)-1
 	groups = as.character(t(data)[,1]) # data.frame to vector by row
 
+	for(i in 1:n) { # 'for' statement start
 		values = as.numeric(t(data)[,i+1])
 
 		# post hoc tests - tukey #
@@ -24,4 +27,6 @@
 			tmp2 = cbind(row[i+1],t(data.frame(sigroup$Letters)))
 			out = cbind(out,tmp2)
 		}
+	} # 'for' statement end
 	return(out)
+} # function 'ss.tukey' end
