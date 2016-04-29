7f1a7f8fb7e8a27e2eebb0d0c579c4689c8c8e29
kisudsoe <kisudsoe@gmail.com>
2016-04-29 13:38:50 +0900

    anova 함수_수정

--- anova.r
+++ anova.r
@@ -1,4 +1,4 @@
-data = input(1) # Set variable ¡®data¡¯ with input data
+data = input(1) # Set variable 'data' with input data
 
 ssanova2 = function(data) { # function start
 	row = rownames(data)
@@ -7,23 +7,23 @@ ssanova2 = function(data) { # function start
 
 	n = length(row)-1
 	groups = as.character(t(data)[,1]) # data.frame to vector by row
-	print(groups) # ¡®print¡¯ frunction to check the process
+	print(groups) # 'print' frunction to check the process
 
-	for(i in 1:n) { # ¡®for¡¯ statement start
+	for(i in 1:n) { # 'for' statement start
 		values = as.numeric(t(data)[,i+1])
 
 		fit = lm(values~groups)
 		result = anova(fit)
 		anovaP = result$'Pr(>F)'[1]
 
-		if(i==1) { # ¡®if¡¯ statement start
+		if(i==1) { # 'if' statement start
 		out = data.frame(row[i+1],anovaP)
-		} else { # ¡®else¡¯ statement start
+		} else { # 'else' statement start
 		tmp = data.frame(row[i+1],anovaP)
 		out = rbind(out,tmp)
-		} # ¡®else¡¯ statement end
-	} # ¡®for¡¯ statement end
+		} # 'else' statement end
+	} # 'for' statement end
 
-	return(out) # return the variable ¡®out¡¯
+	return(out) # return the variable 'out'
 
 } # function end
